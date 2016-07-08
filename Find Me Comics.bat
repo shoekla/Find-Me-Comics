d164359d33e3699209a67d048a5eb23a5ce8edb8
Abir <abirshukla@gmail.com>
2016-07-08 17:14:01 -0500

    Added Download feature

---
+++ Find Me Comics.bat
@@ -0,0 +1,10 @@
+python getpip.py
+pip install flask
+pip install requests
+pip install bs4
+pip install BeautifulSoup
+pip install urllib2
+
+cd code
+start chrome http://127.0.0.1:5000/
+python launch.py
